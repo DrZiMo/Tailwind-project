9ab3d34b9abf0cb4c29295e80d836ef0d27767b0
DrZiMo <zuhaybhamar@gmail.com>
2024-10-25 18:12:58 +0300

    Finished the main part

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lilitaOne: ["Lilita One", "serif"],
+        lexend: ["Lexend", "serif"]
+      }
+    },
   },
   plugins: [],
 }
